ce6982dfdf9df1f8d98fffbb6aa75f95cdceea5d
bagusgants <bagushidayatullah001@gmail.com>
2023-12-22 16:48:13 +0700

    first commit

---
+++ basic/views/site/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Jual Beli Burung</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Jual Beli Burung</h1>
+    </header>
+    <main>
+        <section class="banner">
+            <img src="https://cdn.pixabay.com/photo/2017/02/27/16/38/banner-2103631_1280.jpg" alt="Banner jual beli burung">
+            <style>
+        /* CSS tambahan untuk styling */
+        .bird-img {
+            width: 300px;
+            height: 200px;
+            object-fit: cover;
+            border-radius: 5px;
+            margin-bottom: 15px;
+        }
+    </style>
+            <h2>Burung-burung cantik dan berkualitas</h2>
+            <p>Tersedia berbagai jenis burung, mulai dari burung hias hingga burung kicau</p>
+        </section>
+        <section class="produk">
+            <h2>Produk</h2>
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="https://cdn.britannica.com/11/130511-050-37878416/Burung-lovebird-burung-cinta.jpg" alt="Burung Lovebird">
+                        <h3>Burung Lovebird</h3>
+                        <p>Burung hias yang terkenal dengan warnanya yang indah</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="https://cdn.britannica.com/26/15326-050-76C574E3/Burung-kakatua-parrot-burung-pintar.jpg" alt="Burung Kakatua">
+                        <h3>Burung Kakatua</h3>
+                        <p>Burung cerdas yang dapat dilatih untuk berbicara</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="https://cdn.britannica.com/93/142993-050-D15885B2/Burung-perkutut-burung-peliharaan.jpg" alt="Burung Perkutut">
+                        <h3>Burung Perkutut</h3>
+                        <p>Burung peliharaan yang dipercaya dapat membawa keberuntungan</p>
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section class="kontak">
+            <h2>Kontak</h2>
+            <p>
+                Alamat: Jalan Raya Solo-Semarang Km 10, Semarang
+                Telepon: 0812-3456-7890
+                Email: jualbeliburung@gmail.com
+            </p>
+        </section>
+    </main>
+    <footer>
+        <p>Copyright &copy; 2023 Jual Beli Burung</p>
+    </footer>
+</body>
+</html>
